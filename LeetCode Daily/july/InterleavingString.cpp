87aac58d4cb9ddd77996a2cba61e3a0fc8aef81b
Abhijit Utkarsh <abhijitu18@gmail.com>
2022-07-16 23:47:51 +0530

    weekly301

--- LeetCode Daily/july/InterleavingString.cpp
+++ LeetCode Daily/july/InterleavingString.cpp
@@ -1,30 +1,33 @@
-class Solution {
+class Solution
+{
 public:
     int dp[101][101];
-    bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.length() , m= s2.length() , s = s3.length();
-        memset(dp, -1, sizeof(dp));
-        return helper(s1, s2, s3, 0, 0 ,0 );
-    }
-    
-    bool helper(string s1, string s2, string s3, int i , int j, int k)
+    bool isInterleave(string s1, string s2, string s3)
     {
-        if(i == s1.length() and j== s2.length() and k==s3.length())
+        int n = s1.length(), m = s2.length(), s = s3.length();
+        memset(dp, -1, sizeof(dp));
+        return helper(s1, s2, s3, 0, 0, 0);
+    }
+
+    bool helper(string s1, string s2, string s3, int i, int j, int k)
+    {
+        if (i == s1.length() and j == s2.length() and k == s3.length())
             return true;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        bool x= false, y = false;
-        if(i != s1.length())
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        bool x = false, y = false;
+        if (i != s1.length())
         {
-            if(s1[i] == s3[k])
-            x = helper(s1, s2, s3, i+1, j , k+1);
+            if (s1[i] == s3[k])
+                x = helper(s1, s2, s3, i + 1, j, k + 1);
         }
-        if(j != s2.length())
+        if (j != s2.length())
         {
-            if(s2[j] == s3[k])
-            y = helper(s1, s2, s3, i, j+1 , k+1);
+            if (s2[j] == s3[k])
+                y = helper(s1, s2, s3, i, j + 1, k + 1);
         }
-        return dp[i][j] = x|y;
+        return dp[i][j] = x | y;
     }
 };
